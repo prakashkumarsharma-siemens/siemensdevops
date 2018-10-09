ac494d1dd82395c733f192fbbe4e39ed0b36e542
prakashkumarsharma-siemens <prakashkumar.sharma@siemens.com>
2018-10-09 14:41:07 +0530

    Initial Commit

---
+++ myapp2_Gradle/src/main/java/com/siemens2/myapp2/Myapp2Application.java
@@ -0,0 +1,12 @@
+package com.siemens2.myapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myapp2Application.class, args);
+	}
+}
---
+++ myapp2_Gradle/src/test/java/com/siemens2/myapp2/Myapp2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.siemens2.myapp2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Myapp2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
